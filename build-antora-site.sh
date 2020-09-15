28b2cd8e8836321345d3c962ea318007ae907250
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2020-09-15 18:11:19 +0200

    Remove Error and Warnings Add support for Plotly

--- build-antora-site.sh
+++ build-antora-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-antora antora-local-feelpp-doc.yml
+antora antora-local-feelpp-doc.yml > log 2>&1
 echo "INFO: File generated in 'build/site/feelpp-doc/'"
 npm i -g live-server
 live-server build/site
